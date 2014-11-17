2a9095f4fd6b3f7b2a35f32b455e64dca9d383f3
root <root@localhost.localdomain>
2014-11-17 15:05:18 -0500

    MI PAGINA DE REGALOS

---
+++ portada.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>REGALOS</title>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--[if lte IE 6]><link rel="stylesheet" href="css/ie6.css" type="text/css" media="all" /><![endif]-->
+<script type="text/javascript" src="js/jquery-1.2.3.pack.js"></script>
+<script type="text/javascript" src="js/jquery.jcarousel.pack.js"></script>
+<script type="text/javascript">
+jQuery(document).ready(function() {
+	jQuery('#mycarousel').jcarousel({
+		scroll: 1,
+		animation: "slow",
+		wrap: "both"
+	});
+});
+</script>
+</head>
+<body>
+<!-- Header -->
+<div id="header">
+  <div class="shell">
+    <div class="cl">&nbsp;</div>
+    <!-- Logo -->
+    <div id="logo-cnt">
+      <h1 id="logo">REGALOS DE LA TEMPORADA</h1>
+      <h2 id="slogan">Todo lo hacemos de la manera mas sencilla y facil de comprar</h2>
+    </div>
+    
+    
+    <div class="cl">&nbsp;</div>
+    <!-- Carousel -->
+    <div id="slider">
+      <ul id="mycarousel">
+        <li>
+          <div class="img-cnt"> <a href="mujer.html"><img src="css/images/mujer.jpg" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="hombre.html"><img src="css/images/hombre.jpg" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="navidad.html"><img src="css/images/navidad.jpg" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="bebes.html"><img src="css/images/bebes.jpg" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="ninos.html"><img src="css/images/ninos.jpg" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="hogar.html"><img src="css/images/hogar.jpg" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="juguetes.html"><img src="css/images/juguetes.jpg" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="libros.html"><img src="css/images/libros.jpg" alt="" /> </a> </div>
+        </li>
+      </ul>
+    </div>
+    <!-- End Carousel -->
+  </div>
+</div>
+<!-- End Header -->
+<!-- Main -->
+<div id="main">
+  <div class="shell">
+    <!-- Article -->
+    <div class="article">
+      <h2>AQUI ENCONTRARAS NUESTRAS MEJORES OFERTAS Y PROMOCIONES</h2>
+      <p><h3>En nuestra pagina podras observar nuestras nuevas tendencias del mercado global y damos a conocer las mejores ofertas de cada mes y las grandes promociones que tenemos para ti. Nuestro servicio es de alta calidad y de buenos precios...!</h3></p>
+    </div>
+    <!-- End Article -->
+    <!-- Gallery -->
+    <div class="gallery">
+      <div class="cl">&nbsp;</div>
+      <h2 class="left">QUE DESEAS COMPRAR..?</h2>
+      <a href="" class="view-all"></a>
+      <div class="cl">&nbsp;</div>
+      <div class="cl">&nbsp;</div>
+
+      
+      <table rowspan border="2"> 
+
+      <tr>
+      <td style="vertical-align:middle; text-align:center">
+      <div class="portfolio-item last"> <a href=""><img src="css/images/mujer.jpg" WIDTH=300 HEIGHT= 300 alt="" /></a> <a href="mujer.html">VER</a> </div>
+      </td>
+     
+      <td style="vertical-align:middle; text-align:center">
+      <div class="portfolio-item"> <a href=""><img src="css/images/hombre.jpg" WIDTH=300 HEIGHT= 300alt="" /></a> <a href="hombre.html">VER</a> </div>
+      </td>
+
+      <td style="vertical-align:middle; text-align:center">
+      <div class="portfolio-item"> <a href=""><img src="css/images/ninos.jpg"WIDTH=300 HEIGHT= 300 alt="" /></a> <a href="ninos.html">VER</a> </div>
+      </td>
+
+      <td style="vertical-align:middle; text-align:center">
+      <div class="portfolio-item"> <a href=""><img src="css/images/bebes.jpg" WIDTH=300 HEIGHT= 300alt="" /></a> <a href="bebes.html">VER</a> </div>
+      </td>
+      </tr>
+      <tr>
+
+      <td style="vertical-align:middle; text-align:center">
+      <div class="portfolio-item"> <a href=""><img src="css/images/libros.jpg"WIDTH=300 HEIGHT= 300 alt="" /></a> <a href="libros.html">VER</a> </div>
+      </td>
+
+      <td style="vertical-align:middle; text-align:center">
+      <div class="portfolio-item"> <a href=""><img src="css/images/navidad.JPG"WIDTH=300 HEIGHT= 300 alt="" /></a> <a href="navidad.html">VER</a> </div>
+      </td>
+
+      <td style="vertical-align:middle; text-align:center">
+      <div class="portfolio-item last"> <a href=""><img src="css/images/juguetes.JPG"WIDTH=300 HEIGHT= 300 alt="" /></a> <a href="juguetes.html">VER</a> </div>
+      </td>
+     
+      <td style="vertical-align:middle; text-align:center">
+      <div class="portfolio-item last"> <a href=""><img src="css/images/hogar.jpg"WIDTH=300 HEIGHT= 300  alt="" /></a> <a href="hogar.html">VER</a> </div>
+      </td>
+      </tr>
+
+      <div class="cl">&nbsp;</div>
+    </div>
+    <!-- End Gallery -->
+    <!-- Contact -->
+   </table>
+
+<!-- End Main -->
+<div id="footer">
+  <p>&copy; CREADO POR LOS ESTUDIANTES DE INGENIERIA DE SISTEMAS</p>
+<p>&copy; CORPORACION UNIVERSITARIA LATINOAMERICANA, CUL</p>
+<p>&copy; TODOS LOS DERECHOS RESERVADOS</p>
+</div>
+</body>
+</html>
